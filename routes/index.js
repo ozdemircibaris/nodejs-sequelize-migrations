cec28fe5f255c729dbe27626230bbe2e2815f7c1
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2021-07-07 11:43:14 +0300

    db sync process

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+const { sequelize } = require('../database/db');
+let router = express.Router();
+
+sequelize.sync({ force: false }).then((result) => {
+  console.log("connected db");
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Migrations Project' });
 });
 
 module.exports = router;
